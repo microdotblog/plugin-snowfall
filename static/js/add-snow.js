607f44c5f21482ab548eade51b123b6df622923b
Manton Reece <manton@me.com>
2024-12-19 10:32:08 -0600

    Removed HTMl, replace with JS to dynamically add snow component.

---
+++ static/js/add-snow.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const snowFallElement = document.createElement('snow-fall');
+	snowFallElement.style.setProperty('--snow-fall-color', '#E0F7FA');
+	document.body.appendChild(snowFallElement);
+});
